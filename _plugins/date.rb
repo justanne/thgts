a919e4758fae22a6338c71cf06fcf17544811da0
Anne Barrios <mail.me@annebarrios.me>
2013-02-12 16:50:30 +0800

    removed original

--- _plugins/date.rb
+++ _plugins/date.rb
@@ -3,8 +3,7 @@ require 'facets/integer/ordinal'
 
 module Jekyll
   module DateFilter
-    def prettyday(date)
-      # "#{date.strftime('%e').to_i.ordinalize} #{date.strftime('%B')} #{date.strftime('%Y')}"
+    def prettyday(date)     
       "#{date.strftime('%d').to_i.ordinalize}"
     end  
   end
